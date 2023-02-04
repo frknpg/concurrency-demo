9200b09362493b3f5f5e47bd369e783ae8f74c48
frknpg <frknpg@gmail.com>
2023-02-04 03:47:34 +0300

    With transactional

--- src/main/java/me/frknpg/concurrencyfix/product/ProductService.java
+++ src/main/java/me/frknpg/concurrencyfix/product/ProductService.java
@@ -1,5 +1,6 @@
 package me.frknpg.concurrencyfix.product;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +10,7 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
+    @Transactional
     public void upsert(CreateProductRequest createProductRequest) {
         Product product = productRepository.findByUuid(createProductRequest.getUuid())
                 .orElseGet(() -> Product.builder()
